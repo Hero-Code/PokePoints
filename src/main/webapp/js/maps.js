711f6f067107d54e95bf38a1e007cd8759eec3ec
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-07-16 00:16:19 -0300

    Created Google Maps Developer Key

---
+++ src/main/webapp/js/maps.js
@@ -0,0 +1,8 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var key = "AIzaSyCIudn50CSC1UI8j2GCRvrZi7KimfStgzg";
+
